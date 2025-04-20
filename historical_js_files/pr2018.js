6a2216e79bb3a223bfe4e3bb9169a14e1ea8d4b7
James Canaday <52135018+jwcanaday@users.noreply.github.com>
2025-04-20 11:01:30 -0500

    Add files via upload

---
+++ historical_js_files/pr2018.js
@@ -0,0 +1,24 @@
+var pr2018 = [
+{
+	date: 'December 2018',
+    file: '2018/12/SexualAssaultTaskForceReport.pdf',
+    title: 'Attorney General’s Working Group on Sexual Assault Releases Report and Recommendations',
+    lede: '',
+},{
+	date: 'December 21, 2018',
+    file: '2018/12/21_ComcastXfinity.asp',
+    title: 'Attorney General Lori Swanson Sues Comcast/Xfinity for Charging More Than Promised, Charging for Unordered Products, and Not Delivering Promised Promotional Prepaid Visa Cards.',
+    lede: '“It’s hard to shop for cable television if a company plays hide-the-ball on its true prices, and people shouldn’t have to watch their bills for things they didn’t buy,” said Attorney General Swanson. ',
+},{
+	date: 'October 16, 2018',
+    file: '2018/10/16_InsulinPriceHikes.asp',
+    title: 'Attorney General Lori Swanson Files Lawsuit Against Pharmaceutical Companies Over Deceptive Price Spikes For Insulin',
+    lede: 'Price Hikes More Than Doubled the Cost Of Diabetes Medication',
+},
+	
+	
+];
+
+
+
+	
